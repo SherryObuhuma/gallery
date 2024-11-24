cff40e3e1aa07b530d6a43cbfd40cf8f99ad6a97
SherryObuhuma <andesosherry@gmail.com>
2024-11-24 14:59:31 +0300

    Updated the MongoDB URI on the config file

--- _config.js
+++ _config.js
@@ -2,9 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    constant uri = "mongodb+srv://sherry:Macherie2015@cluster1.mabot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+    URI:"mongodb+srv://sherry:Macherie2015@cluster1.mabot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1",
     production: 'mongodb+srv://sherry:Macherie2015@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://sherry:Macherie2015@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://sherry:Macherie2015@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
-module.exports = config;
+module.exports = {
+    URI,
+};
