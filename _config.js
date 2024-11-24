e685b2a9e631f70e155b1d33ceed52f8d8966227
SherryObuhuma <andesosherry@gmail.com>
2024-11-24 14:48:03 +0300

    Modified the config.js file for the test branch

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    mongodb+srv://sherry:Macherie2015@cluster1.mabot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1
+    constant uri = "mongodb+srv://sherry:Macherie2015@cluster1.mabot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
     production: 'mongodb+srv://sherry:Macherie2015@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://sherry:Macherie2015@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://sherry:Macherie2015@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
